7cbbd9c93a2ec769e0d51d8c8078f5868127ec39
dastangoo <hamed.dastangoo@gmail.com>
2017-09-08 00:45:49 +0430

    Building a Nested Component

---
+++ APM-Start/src/app/shared/star.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnChanges } from '@angular/core';
+
+@Component({
+    selector: 'ai-star',
+    moduleId: module.id,
+    templateUrl: 'star.component.html',
+    styleUrls: ['star.component.css']
+})
+export class StarComponent implements OnChanges {
+    starWidth: number;
+    rating: number = 4;
+    
+    ngOnChanges(): void {
+        this.starWidth = this.rating * 86/5;
+    }
+}
