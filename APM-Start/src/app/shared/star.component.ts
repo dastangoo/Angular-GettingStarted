a8316a0973611e2bc67d68a69efb6a913f04d2fb
dastangoo <hamed.dastangoo@gmail.com>
2017-09-08 01:03:38 +0430

    Passing Data to a Nested Component Using @Input

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 
 @Component({
     selector: 'ai-star',
@@ -8,7 +8,7 @@ import { Component, OnChanges } from '@angular/core';
 })
 export class StarComponent implements OnChanges {
     starWidth: number;
-    rating: number = 4;
+    @Input() rating: number;
     
     ngOnChanges(): void {
         this.starWidth = this.rating * 86/5;
