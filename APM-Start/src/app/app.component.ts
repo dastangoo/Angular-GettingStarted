382abef73b4369c072bca2403ff5009edfadd22c
dastangoo <hamed.dastangoo@gmail.com>
2017-09-07 19:35:06 +0430

    Binding with interpolations

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'pm-app',
   template: `
   <div><h1>{{pageTitle}}</h1>
+    <h1 innerText={{pageTitle}}></h1>
     <pm-products></pm-products>
   </div>
   `
