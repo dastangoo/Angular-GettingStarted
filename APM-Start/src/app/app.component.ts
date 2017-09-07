e66309ba88ed8925f2a8869519bf4f15103dccd5
dastangoo <hamed.dastangoo@gmail.com>
2017-09-07 22:45:41 +0430

    Defining Interfaces

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'pm-app',
   template: `
   <div><h1>{{pageTitle}}</h1>
-    <h1 innerText={{pageTitle}}></h1>
     <pm-products></pm-products>
   </div>
   `
